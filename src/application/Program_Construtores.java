053f092bb6ef654aa6ded1762d264ab49034e91b
MaianeSamara <maianesamara@gmail.com>
2022-11-21 18:34:19 -0300

    Melhoria Atividade Construtores

--- src/application/Program_Construtores.java
+++ src/application/Program_Construtores.java
@@ -12,26 +12,26 @@ public class Program_Construtores {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product_Construtores product = new Product_Construtores();
-		
 		//Atributos "Vazios"
-		System.out.println(product.name);
-		System.out.println(product.price);
-		System.out.println(product.quantity);
+//		System.out.println(product.name);
+//		System.out.println(product.price);
+//		System.out.println(product.quantity);
 		
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
-		product.name = sc.next();
-		
+		String name = sc.next();		
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
-		
+		double price = sc.nextDouble();	
 		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
+		Product_Construtores product = new Product_Construtores(name, price, quantity);
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
 
 		System.out.println();
 		System.out.print("Enter the number os products to be added in stock: ");
-		int quantity = sc.nextInt();
+		quantity = sc.nextInt();
 		product.addProducts(quantity);
 		
 		System.out.println();
