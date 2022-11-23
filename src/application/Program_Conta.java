72acfbef8134e63a61f467ddfa54cb2ce7f76ff1
MaianeSamara <maianesamara@gmail.com>
2022-11-23 16:53:23 -0300

    Conta - Exercicio de Fixação

---
+++ src/application/Program_Conta.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Conta;
+
+public class Program_Conta {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Conta conta;
+		
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		
+		System.out.print("Enter account holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Is there as initial deposit (y/n): ");
+		char response = sc.next().charAt(0);
+		if(response == 'y') {
+			System.out.print("Enter initial value: ");
+			double initialDeposit = sc.nextDouble();
+			conta = new Conta(number, holder, initialDeposit);
+		}
+		else {
+			conta = new Conta(number, holder);
+		}
+		
+		System.out.println();
+		System.out.println("Account data:");
+		System.out.println(conta);
+		
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		conta.deposit(depositValue);
+		System.out.println("Update account data: ");
+		System.out.println(conta);
+
+		System.out.println();
+		System.out.print("Enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		conta.withdraw(withdrawValue);
+		System.out.println("Update account data: ");
+		System.out.println(conta);
+		
+		sc.close();
+	}
+
+}
