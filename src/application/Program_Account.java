fac59ff8cdf472e51c95e315073c37c6ea743ca5
MaianeSamara <maianesamara@gmail.com>
2022-12-13 16:06:30 -0300

    Exemplo Herança

---
+++ src/application/Program_Account.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program_Account {
+
+	public static void main(String[] args) {
+	
+		
+		BusinessAccount account = new BusinessAccount();
+	}
+
+}
